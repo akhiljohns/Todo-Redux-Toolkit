3665747defc00b1b9533ab955d9c4c05b8c83827
Akhil Johns <akhiljohns250@gmail.com>
2023-12-22 10:04:13 +0530

    Finished Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
-import "./styles.css";
-
-export default function App() {
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import Todo from "./Todo";
+const App = () => {
   return (
-    <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input typ e="text" id="item" />
-        </div>
-      </form>
-
-      <h1 className="header">Todo List</h1>
-      <ul className="list">
-        <li>
-          <label htmlFor="item1">
-            <input type="checkbox" id="item1" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label htmlFor="item2">
-            <input type="checkbox" id="item2" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-      </ul>
-    </>
+    <Provider store={store}>
+       <Todo /> 
+    </Provider>
   );
-}
+};
+export default App;
